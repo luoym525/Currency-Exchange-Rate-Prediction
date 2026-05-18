91c334bb6f6cc5622f592f8b59f09e273d796521
罗翊铭 <luoyiming525@hotmail.com>
2026-05-18 12:06:55 +0800

    删除 config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-    def __init__(self):
-        pass
-    @staticmethod
-    def _get_Fred_api():
-        return "50ef49a76c6f8d6e06c3f7f6a0f703ac"
-    @staticmethod
-    def _get_dataset_location():
-        return r"D:\code\CNY_GBP_exchange_rate_prediction\dataset"
