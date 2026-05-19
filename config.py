e637bd0cd18490ec6924fd6607496d9a44289507
罗翊铭 <luoyiming525@hotmail.com>
2026-05-19 17:38:51 +0800

    1

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+    def __init__(self):
+        pass
+    @staticmethod
+    def _get_Fred_api():
+        return r"your api key"
+    @staticmethod
+    def _get_dataset_location():
+        return r"your dataset location"
+    @staticmethod
+    def _get_save_directory():
+        return r"path to where you want to save your model data"
